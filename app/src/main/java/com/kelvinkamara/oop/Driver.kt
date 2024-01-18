2041b3be7e598076a49a9366ed3cc2a7d70f3cae
kkamara <kamaracomputers@gmail.com>
2024-01-18 16:37:54 +0000

    Remove commented code

--- app/src/main/java/com/kelvinkamara/oop/Driver.kt
+++ app/src/main/java/com/kelvinkamara/oop/Driver.kt
@@ -3,13 +3,9 @@ package com.kelvinkamara.oop
 import android.util.Log
 
 class Driver(var name: String, credit: Int) {
-//    var driverName = ""
-//    protected lateinit var driverName: String
-//    var driverName = name
     val car = Car()
 
     init {
-//        driverName = name
         car.maxSpeed = 150
         car.start()
     }
